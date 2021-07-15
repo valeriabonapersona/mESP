6a90d35447074027620b189cd4a8f0d11b07703c
valeriabonapersona <valeria.bonapersona@gmail.com>
2021-07-15 17:46:32 +0200

    new analyses links

--- run.R
+++ run.R
@@ -2,3 +2,12 @@
 
 'Prepare a temporary dataframe for further processing for each outcome separately'
 source("src/data_preparation.R")
+
+
+
+# analyses
+source("src/analysis_size.R")
+source("src/analysis_brdu.R")
+source("src/analysis_dcx.R")
+source("src/analysis_ki67.R")
+source("src/analysis_spines.R")
