02613f8c1cec28acca26fead904b526cdaf4ae10
valeriabonapersona <valeria.bonapersona@gmail.com>
2021-07-28 16:21:07 +0200

    sex and species added

--- config/config.R
+++ config/config.R
@@ -18,7 +18,7 @@ final <- "data/processed/"
 
 # Other vars --------------------------------------------------------------
 life_vars <- c("cite", "link", "id", "exp_id", 
-               "sex", "age_testing_weeks", "model", "origin", 
+               "sex", "species", "strain", "age_testing_weeks", "model", "origin", 
                "housing_after_weaning", "behavior", "major_life_events", 
                "at_death")
 
