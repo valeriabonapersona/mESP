85b73e63cb3b1b06fb07db18ea85618544199a18
valeriabonapersona <valeria.bonapersona@gmail.com>
2021-07-28 16:21:15 +0200

    grid in background

--- config/utilities.R
+++ config/utilities.R
@@ -25,10 +25,11 @@ my_theme <- theme_classic() +
   theme(plot.title = element_text(face = "bold", hjust = 0.5, margin = margin(t = 20, r = 0, b = 10, l = 0)),
         axis.title.x = element_text(hjust = 0.95),
         axis.title.y = element_text(hjust = 0.95),
+        panel.grid.major = element_line(colour = "grey95"), 
+        panel.grid.minor = element_line(colour = "grey95"),
         plot.margin = unit(c(0.5,0.5,0.5,0.5), "cm")) 
 
 
 std_fill <- scale_fill_viridis(discrete = TRUE)
 my_bin <- 50
 my_bin_width <- my_bin / 5000
-
